076730ec9b49dd6bb81f4a626f20632dc145e4bc
Thomas Unterreitmeier <t.unterreitmeier@icloud.com>
2020-05-08 18:11:02 +0200

    particle collision checking

--- ts/index.ts
+++ ts/index.ts
@@ -8,13 +8,29 @@ let center = {
   y: canvas.canvas.height / 2
 }
 
-let mouseCircle = new Circle({x: 50, y: 50}, 50, 'teal', canvas);
+let mouseCircle = new Circle({x: 50, y: 50}, 15, 'teal', canvas);
 
 let obstacles:Array<Circle> = [];
-for(let i = 0; i < 10; i++) {
+let colors = ['MidnightBlue', 'SteelBlue'];
+for(let i = 0; i < 60; i++) {
+  // randomize speed and location of circles
+  let r = 10;
   let dx = (Math.random() - 0.5) * 8;
   let dy = (Math.random() - 0.5) * 8;
-  let obstacle = new Circle({x: center.x, y: center.y}, 50);
+  let x = Math.random() * (canvas.canvas.width - 2 * r) + r;
+  let y = Math.random() * (canvas.canvas.height - 2 * r) + r;
+  let color = colors[Math.floor(Math.random() * colors.length)];
+  let obstacle = new Circle({x: x, y: y}, r, color);
+  let goodSpot = true;
+  for (let other of obstacles) {
+    if(obstacle.getDistance(other) < 0 && obstacle != other) {
+      goodSpot = false;
+    }
+  }
+  if(!goodSpot) {
+    i--;
+    continue;
+  }
   obstacles.push(obstacle);
   obstacle.linearMove({x: dx, y: dy}, true)
   canvas.addElement(obstacle);
@@ -25,13 +41,19 @@ let test = new Rectangle({x: 200,y: 300}, 50, 300, 'orange');
 
 //canvas.addElement(mouseCircle);
 canvas.addElement(test);
-//canvas.draw();
+canvas.draw();
 
 canvas.animate(() => {
   // this is called on every frame
 
   let touched = false;
   for (let obstacle of obstacles) {
+    for (let other of obstacles) {
+        if(other != obstacle && obstacle.getDistance(other) < 0) {
+          //one of the particles touched
+          
+        }
+    }
     if(mouseCircle.getDistance(obstacle) <= 0) {
       touched = true;
     }
